aab8bd89ca912b053b618e9cc8486d0ce34d329d
Uttam Negi <piyush.negi@kalvium.community>
2023-05-19 19:55:55 +0530

    navbar changed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,30 @@
-import { Spin as Hamburger } from "hamburger-react";
-import { useState } from "react";
 import "./Navbar.scss";
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
   return (
-    <div className="navbar">
-      <h3>
-        <img src="./logo.png" alt="logo" />
-        iyush
-      </h3>
-      <div className={menuOpen ? 'menu-open' : 'menu'}>
-          <ul>
-            <li data-text="HOME">HOME</li>
-            <li data-text="ABOUT">ABOUT</li>
-            <li data-text="PROJECTS">PROJECTS</li>
-            <li data-text="SERVICES">SERVICES</li>
-          </ul>
-          <button>Contact Me</button>
+    <div className="n-wrapper">
+      <div className="left">
+        <div>Piyush</div>
+        <span>toggle</span>
       </div>
-      <Hamburger onToggle={() => setMenuOpen(!menuOpen)} />
+      <div className="right">
+        <div>
+          <ul>
+            <li data-replace='Home'>Home</li>
+            <li data-replace='Services'>Services</li>
+            <li data-replace='Projects'>Projects</li>
+            <li data-replace='Testimonials'>Testimonials</li>
+          </ul>
+        </div>
+
+        <a href="#" className="cta">
+          <span>Contact me</span>
+          <svg width="13px" height="10px" viewBox="0 0 13 10">
+            <path d="M1,5 L11,5"></path>
+            <polyline points="8 1 12 5 8 9"></polyline>
+          </svg>
+        </a>
+      </div>
     </div>
   );
 };
